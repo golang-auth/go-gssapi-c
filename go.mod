0338a12fdb1012d83ae27d359c34d72b450ea76a
Jake Scott <jake@jakethesnake.dev>
2024-08-19 20:43:41 -0400

    Moved from go-gssapi

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/golang-auth/go-gssapi-c
+
+go 1.18
+
+replace github.com/golang-auth/go-gssapi/v3 => ../go-gssapi/v3
+
+require (
+	github.com/golang-auth/go-gssapi/v3 v3.0.0-00010101000000-000000000000
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ provider.go
@@ -0,0 +1,29 @@
+package gssapi
+
+import (
+	"errors"
+
+	g "github.com/golang-auth/go-gssapi/v3"
+)
+
+// #cgo LDFLAGS: -lgssapi_krb5
+import "C"
+
+
+const LIBID = "GSSAPI-C"
+
+func init() {
+	g.RegisterProvider(LIBID, New)
+}
+
+type library struct {
+	name string
+}
+
+func New() g.Provider {
+	return &library{
+		name: LIBID,
+	}
+}
+
+var ErrTooLarge = errors.New("The GSSAPI-C bindings only support up to 32 bit messages")
